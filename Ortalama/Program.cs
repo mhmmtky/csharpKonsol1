3dbc8974047b89b5af90ef977ed9ddcec21652ac
mhmmtky <mhmmtky78@gmail.com>
2024-11-20 23:29:33 +0300

    csharpKonsol1

---
+++ Ortalama/Program.cs
@@ -0,0 +1,13 @@
+﻿int sayi1;
+int sayi2;
+
+Console.WriteLine("1. sayıyı giriniz:");
+sayi1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("1. sayı = "+sayi1);
+Console.WriteLine("2. sayıyı giriniz:");
+sayi2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2.sayı = "+sayi2);
+
+int ort = (sayi1+sayi2)/2;
+
+Console.WriteLine("Ortalama ="+ ort);
---
+++ adSoyad/Program.cs
@@ -0,0 +1,18 @@
+﻿string ad;
+string soyAd;
+int ogNo;
+double telNo;        //System.OverflowException: Value was either too large or too small for an Int32 değişkeni int olarak atadığımda bu hatayı aldığım için double ile atadım hocam.
+                    
+
+Console.WriteLine("İsminizi Giriniz:");
+ad = Console.ReadLine();
+Console.WriteLine("Soy İsminizi Giriniz");
+soyAd = Console.ReadLine();
+Console.WriteLine("Öğrenci Numaranızı Giriniz");
+ogNo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Telefon Numaranızı Giriniz");
+telNo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("İsim Soyisminiz : "+ad + " " + soyAd);
+Console.WriteLine("Öğrenci Numaranız : "+ ogNo);
+Console.WriteLine("Telefon Numaranız : "+ telNo);
